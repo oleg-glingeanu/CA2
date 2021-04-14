9310c95d2dfbc055ee19d9fefe8bff897b786288
Oleg Glingeanu <oleg.glingeanu@gmail.com>
2021-04-14 19:07:37 +0100

    Secret Key update

--- skate_shop/settings.py
+++ skate_shop/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6++rdea0yuu#$*x9h*731k8$i*=)55is(_g%q$b^du+33+$coq'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
